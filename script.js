ae3d929906261a78def016dc16a0f3a72b9f8f2d
Diego Serradilla <diego.serradilla@alu.ceacfp.es>
2026-03-30 21:22:21 +0200

    Añadidas tareas con el localStorage

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-
-let tareas = [];
+//1. VARIABLES
+let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
 
 
 const boton = document.getElementById("addBtn");
 const input = document.getElementById("taskInput");
-const section = document.querySelector("section");
+const listaTareas = document.getElementById("listaTareas");
 
 
-boton.addEventListener("click", function() {
-    const texto = input.value;
-
-  
-
-    if (texto === "")  {
-        return;
-    } 
-
-      tareas.push(texto);
-
-      function pintarTareas() {
-        section.innerHTML = "";
+//2.FUNCIONES
+  function pintarTareas() {
+        listaTareas.innerHTML = "";
 
         for (let i = 0 ; i < tareas.length; i++) {
             const nuevaTarea = document.createElement("p");
             nuevaTarea.textContent = tareas[i];
 
             nuevaTarea.addEventListener("click", function () {
-                nuevaTarea.classList.toggle("completada");
+                tareas.splice(i, 1);
+                localStorage.setItem("tareas", JSON.stringify(tareas)); //guardar al borrar
+                pintarTareas();
+
             });
         
-            section.appendChild(nuevaTarea);
+            listaTareas.appendChild(nuevaTarea);
         
-}
+ }
+ } 
+//3.EVENTOS
+boton.addEventListener("click", function() {
+    const texto = input.value;
+    
+    if (texto === "")  {
+        return;
+    }
 
-
+    tareas.push(texto);
+    localStorage.setItem("tareas", JSON.stringify(tareas));  //para guardar en el local stroage, y convertir elm array en texto
     input.value = "";
+    pintarTareas();
 
+});
 
-
-    console.log(tareas);
-
-}
+pintarTareas();
